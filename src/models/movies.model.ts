3d730bb6a4738314653364cec06c50fadf95d549
MarkPari <marco.parisi@stevejobs.academy>
2022-03-21 10:27:41 +0100

    feat(document): start to try to filter

--- src/models/movies.model.ts
+++ src/models/movies.model.ts
@@ -1,12 +1,16 @@
 export interface Movie {
-    id: string;
     adult: boolean;
-    release_date: string;
-    poster_path: string;
-    trailer: string;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
     original_language: string;
-    title: string;
+    original_title: string;
+    overview: string;
     popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
     vote_average: number;
     vote_count: number;
 }
@@ -14,8 +18,6 @@ export interface Movie {
 export interface Movies {
     page: number;
     results: Movie[];
-    total_pages: number;
-    total_results: number;
 }
 
 export interface SingleMov {
@@ -35,3 +37,4 @@ export interface RootObject {
     id: number;
     results: SingleMov[];
 }
+
--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -4,6 +4,7 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { formatCollection } from '../utils';
 import axios from "axios";
 import { Movie, SingleMov } from '../models/movies.model';
+import { Genre } from "../models/genre";
 
 const serviceAccount = require('../../config.json'); //per configurare il project_id in firebase
 
@@ -12,6 +13,17 @@ const router = express.Router();
 
 initializeApp({credential: cert(serviceAccount)}); //per utilizzarlo
 const db = getFirestore();
+
+router.get('/', async({query: {genre}},res)=>{
+    
+    const films =  await axios.get<Movie[]>(`https://api.themoviedb.org/3/discover/movie?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=It&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate`);
+    if(genre){ 
+        const genders: Genre[] =  await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=en-US`);
+        const filterGenre = genders.filter(({name})=>name==genre).map(({id})=>id)
+        films.data.filter(({genre_ids})=>genre_ids[0]== filterGenre[0])
+    }
+    res.json(films.data);
+})
 
 /*router.get('/', async({query: {title}}, res) => {
     const films = db.collection("Films");
@@ -42,10 +54,7 @@ router.get('/', (_,res)=>{
     res.json(resp);
 })*/
 
-router.get('/axios', async(_,res)=>{
-    const films =  await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=It&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate`);
-    res.json(films.data);
-})
+
 
 router.get('/axios/:id', async({params: {id}}, res)=>{
     const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=It`);

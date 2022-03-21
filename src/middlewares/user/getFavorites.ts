5642618bc7a0ac2c08befd23a5302f346588bae7
atria10 <mario.atria@stevejobs.academy>
2022-03-21 19:41:19 +0100

    Feat(users.ts and middlware getFavorites.ts) implemented the possibility to see user's favorites movies

---
+++ src/middlewares/user/getFavorites.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { getFirestore } from "firebase-admin/firestore";
+import { Favorite } from "../../models/favorites.model";
+import { UserSignup } from "../../models/user.model";
+import { formatCollection } from '../../utils';
+
+const db = getFirestore();
+
+export const getFavorites = (async (_: Request, res: Response, next: any) => {
+    let { username } = res.locals.user;
+    
+    const favorites = formatCollection(await db.collection("Favorites").get());
+    if (favorites.some((favorite) => favorite.username === username)) {
+        let userFavorites = favorites.filter(( favorite) => favorite.username === username)
+        res.locals.username= username
+        res.locals.userFavorites = userFavorites;
+        next();
+
+    } else {
+        return res.json({ message: `${username} has not favorites movies yet` });
+    }
+
+})
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userInfo } from "../middlewares/auth/userInfo";
 import { addFavorite } from "../middlewares/user/addFavorites";
+import { getFavorites } from "../middlewares/user/getFavorites";
 import { Favorite } from "../models/favorites.model";
 
 const router = express.Router();
@@ -13,5 +14,12 @@ router.post(`/favorites`,userInfo,addFavorite,(_,res)=>{
     return res.status(200).json({ message: `${favorite.username} added to favorites movie with id:${favorite.movieId}`,
      favorite: favorite})
 })
+router.get(`/favorites`,userInfo,getFavorites,(_,res)=>{
+
+    return res.status(200).json({ message: `${res.locals.username} favorites movies:`,
+     favorites:res.locals.userFavorites })
+})
 
 export default router;
+
+

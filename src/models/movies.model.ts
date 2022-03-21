747519ab27a084b98e52ac278029a80bcfd5d3e2
MarkPari <marco.parisi@stevejobs.academy>
2022-03-21 09:04:47 +0100

    feat(document): incomplete modify

--- src/models/movies.model.ts
+++ src/models/movies.model.ts
@@ -1,23 +1,12 @@
 export interface Movie {
+    id: string;
     adult: boolean;
-    backdrop_path: string;
-    genre_ids: number[];
-    id: number;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
     release_date: string;
+    poster_path: string;
+    trailer: string;
+    original_language: string;
     title: string;
-    video: boolean;
+    popularity: number;
     vote_average: number;
-    vote_count: number;
-}
-
-export interface Movies {
-    page: number;
-    results: Movie[];
-    total_pages: number;
-    total_results: number;
+    backdrop_path: string;
 }

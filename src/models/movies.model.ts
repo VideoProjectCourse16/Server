7df9e64720fa7a5ab2643569591c2cd766cc6e3f
MarkPari <marco.parisi@stevejobs.academy>
2022-03-21 09:36:28 +0100

    feat(document): update after pull

--- src/models/movies.model.ts
+++ src/models/movies.model.ts
@@ -1,12 +1,41 @@
 export interface Movie {
-    id: string;
     adult: boolean;
-    release_date: string;
-    poster_path: string;
-    trailer: string;
-    original_language: string;
-    title: string;
-    popularity: number;
-    vote_average: number;
     backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+export interface Movies {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
+}
+
+export interface SingleMov {
+    iso_639_1: string;
+    iso_3166_1: string;
+    name: string;
+    key: string;
+    site: string;
+    size: number;
+    type: string;
+    official: boolean;
+    published_at: Date;
+    id: string;
+}
+
+export interface RootObject {
+    id: number;
+    results: SingleMov[];
 }
--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -3,7 +3,7 @@ import { getFirestore }  from "firebase-admin/firestore";
 import { initializeApp, cert } from "firebase-admin/app";
 import { formatCollection } from '../utils';
 import axios from "axios";
-import { Movie } from '../models/movies.model';
+import { Movie, SingleMov } from '../models/movies.model';
 
 const serviceAccount = require('../../config.json'); //per configurare il project_id in firebase
 
@@ -13,12 +13,12 @@ const router = express.Router();
 initializeApp({credential: cert(serviceAccount)}); //per utilizzarlo
 const db = getFirestore();
 
-router.get('/', async({query: {title}}, res) => {
+/*router.get('/', async({query: {title}}, res) => {
     const films = db.collection("Films");
     let resp =  formatCollection(await films.get());
     title && (resp=resp.filter(item => item.title.includes(title)));
     res.json(resp);
-})
+})*/
 
 
 router.post('/', async ({body: {title, description}}, res) => {
@@ -32,21 +32,21 @@ router.post('/', async ({body: {title, description}}, res) => {
     res.json({message: 'film aggiunto'});
 })
 
-router.get('/', async(_,res)=>{
+/*router.get('/', async(_,res)=>{
     const films = db.collection("Films");
     let resp = formatCollection(await films.get());
     res.json(resp);
-})
+})*/
 
 router.get('/axios', async(_,res)=>{
     const films =  await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=It&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate`);;
     res.json(films);
 })
 
-/*router.get('/axios/:id', async({params: {id}}, res)=>{
+router.get('/axios/:id', async({params: {id}}, res)=>{
     const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=It`);
     const videoid = data.results.filter((item: SingleMov)=>item.type.includes("Trailer"));
     res.json(`https://www.youtube.com/watch?v=${videoid[0].key}`)
-})*/
+})
 
 export default router;

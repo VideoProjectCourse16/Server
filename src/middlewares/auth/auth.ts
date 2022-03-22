1feefdc706546467021c1467220df0962e588f0c
MarkPari <marco.parisi@stevejobs.academy>
2022-03-22 13:08:37 +0100

    feat(document): merge with jwt branch

--- src/middlewares/auth/auth.ts
+++ src/middlewares/auth/auth.ts
@@ -12,23 +12,4 @@ export const auth = (async ({headers: {token}}: Request, res: Response, next: Ne
     } catch(e){
         res.status(401).json({error: 401, message: "not authorized"})
     }
-    /*res.locals.username;
-    let {name,surname,username}: UserSignup=res.locals.user;
-    let password = res.locals.hashedPassword
-    const users = formatCollection<User>(await db.collection("Users").get());
-    if ( users.some(({username}) =>username === res.locals.username)) {
-        return res.status(401).json({message:`Username ${username} already exists`});
-    } else {
-        const max = Math.max(...users.map(({ id }) => Number(id)) as number[]) + 1;
-        const docRef = db.collection('Users').doc(String(max));
-        await docRef.set({
-            name,
-            surname,
-            username,
-            password
-        })
-        res.locals.username = username;
-        res.locals.user={name,surname,username};
-        next();
-    }*/
 })

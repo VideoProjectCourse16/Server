089ce3056d966b71e4ff6d85150b26cda103d7ea
MarkPari <marco.parisi@stevejobs.academy>
2022-03-24 16:02:49 +0100

    feat(document): try for heroku

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,22 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use('/movies', movies);
-app.use('/auth', auth);
-app.use('/users', users);
+//app.use('/movies', movies);
+//app.use('/auth', auth);
+//app.use('/users', users);
+
+app.get(`/`, auth, (_, res) => {
+    return res.status(200).json({message: "'ok"});
+});
 
 
 //const getFilms='https://api.themoviedb.org/3/discover/movie?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate'
 
+// puoi provare a fare una cosa, è drastica ma per iniziare bisogna farlo
+// rimuovi tutti gli endpoint e le route, ne lasci solo una quì per vedere se funziona
 
 app.listen(port ,()=>console.log(`Project Work Server Is Running, port: ${port}`));
-
+// mandato su telegram
 export default app;
 
 // fai il deploy su heroku adesso!

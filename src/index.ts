4c84e0d87631da86b366c8af992b695bf043f421
MarkPari <marco.parisi@stevejobs.academy>
2022-03-24 15:33:12 +0100

    fix(Procfile): correct path

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,22 @@ import users from "./routes/users";
 
 
 const app = express();
-const port=process.env.port || 3000;
+const port = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
 
-app.use('/movies', movies)
-app.use('/auth', auth)
-app.use('/users', users)
+app.use('/movies', movies);
+app.use('/auth', auth);
+app.use('/users', users);
 
 
 //const getFilms='https://api.themoviedb.org/3/discover/movie?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate'
 
 
-app.listen(port,()=>console.log(`Project Work Server Is Running, port: " ${port}`));
+app.listen(port ,()=>console.log(`Project Work Server Is Running, port: " ${port}`));
+
+export default app;
+
+// fai il deploy su heroku adesso!

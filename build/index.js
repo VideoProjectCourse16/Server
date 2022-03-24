9d0f8026fed0fa2dea10aeccdd795e1529952fc3
MarkPari <marco.parisi@stevejobs.academy>
2022-03-24 16:13:24 +0100

    feat(document): try for heroku

--- build/index.js
+++ build/index.js
@@ -11,16 +11,14 @@ var port = process.env.PORT || 3001;
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
 app.use(cors());
-//app.use('/movies', movies);
-//app.use('/auth', auth);
-//app.use('/users', users);
+app.use('/movies', movies);
+app.use('/auth', auth);
+app.use('/users', users);
 app.get("/", auth_1.default, function (_, res) {
     return res.status(200).json({ message: "'ok" });
 });
 //const getFilms='https://api.themoviedb.org/3/discover/movie?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate'
-// puoi provare a fare una cosa, è drastica ma per iniziare bisogna farlo
-// rimuovi tutti gli endpoint e le route, ne lasci solo una quì per vedere se funziona
+
 app.listen(port, function () { return console.log("Project Work Server Is Running, port: ".concat(port)); });
-// mandato su telegram
+
 exports.default = app;
-// fai il deploy su heroku adesso!
--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,12 @@ app.use(express.json());
 app.use(cors());
 
 
-//app.use('/movies', movies);
-//app.use('/auth', auth);
-//app.use('/users', users);
-
-app.get(`/`, auth, (_, res) => {
-    return res.status(200).json({message: "'ok"});
-});
-
+app.use('/movies', movies);
+app.use('/auth', auth);
+app.use('/users', users);
 
 //const getFilms='https://api.themoviedb.org/3/discover/movie?api_key=d0e9057c846dee14124bd893b0ecdbfd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate'
 
-// puoi provare a fare una cosa, è drastica ma per iniziare bisogna farlo
-// rimuovi tutti gli endpoint e le route, ne lasci solo una quì per vedere se funziona
-// perfetto, fai un push
-
 app.listen(port ,()=>console.log(`Project Work Server Is Running, port: ${port}`));
-// mandato su telegram
-export default app;
 
-// fai il deploy su heroku adesso!
+export default app;

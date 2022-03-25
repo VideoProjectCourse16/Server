5c676c9bea069a41d0df7c4dfc9669020455f7b1
atria10 <mario.atria@stevejobs.academy>
2022-03-25 19:55:44 +0100

    feat(admins.ts) implemented the possibility to delete a movie Not completed

--- src/routes/admins.ts
+++ src/routes/admins.ts
@@ -1,14 +1,15 @@
 import express, { Request } from "express";
 import db from "../connection/connection";
+import { Favorite } from "../models/favorites.model";
 import { Movie } from "../models/movies.model";
 import { formatCollection } from "../utils";
 import router from "./movies";
 
 router.post(`/films`, async ({ body: movie }: Request<{}, {}, Movie>, res) => {
     const movies = formatCollection<Movie>(await db.collection("Films").get());
-    const hasSameType=  JSON.stringify(Object.keys(movie).sort()) === JSON.stringify(Object.keys(movies[0]).sort());
+    const hasSameType = JSON.stringify(Object.keys(movie).sort()) === JSON.stringify(Object.keys(movies[0]).sort());
     const isIdPresent = movies.some(({ id }) => id === movie.id);
-    if(hasSameType){
+    if (hasSameType) {
         if (isIdPresent) {
             res.status(400).json({ error: 400, message: `Operation blocked, ID: ${movie.id} is already present` })
         } else {
@@ -18,9 +19,27 @@ router.post(`/films`, async ({ body: movie }: Request<{}, {}, Movie>, res) => {
             })
             res.status(200).json({ message: `Movie inserted correctly!`, movie: movie });
         }
-    }else{
+    } else {
         res.status(400).json({ error: 400, message: `Operation blocked, Please fill in all the required fields` })
     }
+
+})
+
+router.delete(`/films/:movieId`, async ({ params: { movieId } }: Request<{ movieId: string }, {}, {}, {}>, res) => {
+    const movies = formatCollection<Movie>(await db.collection("Films").get());
+    const index = movies.findIndex(({ id }) => id === movieId);
+    const favoriteMovies = formatCollection<Favorite>(await db.collection("Favorites").get());
+    const indexFavorite = favoriteMovies.findIndex(({ movieId:id }) => id === Number(movieId));
+    if (index > -1) {
+        //questo non se lo prende
+        if(indexFavorite > -1){
+            db.collection('Favorites').doc(favoriteMovies[index].id).delete();
+        }
+        //
+        db.collection('Films').doc(movies[index].id).delete();
+        return res.status(200).json({ message: `Movie with ID: ${movies[index].id} removed` })
+    }
+        return res.json({ error: "404", message: `Movie with ID: ${movieId} not found` })
     
 })
 
